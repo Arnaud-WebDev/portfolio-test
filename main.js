e98ae785a5cc124b2ba5963802e2907ae3acf1dc
Arnaud-WebDev <arnaud.grandpierre@gmail.com>
2023-04-18 22:50:26 +0200

    Ajout d'image, bakground, création de la section home et a propos de moi + photos

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 let menu = document.querySelector("#menu-icon")
 let navbar = document.querySelector(".navbar")
 
-menu.addEventListener('click',()  => {
+/* Au click fait apparaitre le menu et change le burger en croix */
+menu.addEventListener("click", () => {
   navbar.classList.toggle("active")
   menu.classList.toggle("toggle")
 })
+
+/* Retire la barre de navigation au scroll ? */
+window.addEventListener("scroll", () => {
+  navbar.classList.remove("active")
+  menu.classList.remove("toggle")
+})
+
+/* 
+window.onscroll = () => {
+  navbar.classList.remove("active")
+  menu.classList.remove("toggle")
+}
+ */
